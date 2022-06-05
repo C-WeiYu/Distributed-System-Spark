bbdb96a261c7e6105ec30be20d079a5c81a00303
aaron1aaron2 <46644821+aaron1aaron2@users.noreply.github.com>
2022-06-06 00:32:46 +0800

    Update bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,30 +1,20 @@
 #!/bin/bash
-set -xe # 印出執行的命令，執行中有問題則終止
+set -xe
 
-sudo apt-get -y update 
-sudo apt-get -y upgrade
+IP=$1
 
-
-# 設定 IP ====================================================
-IP=$1 # 傳入的第一個參數
-masterIP=$2
-numNodes=$3
-
-sudo sed -i '/127.0.2.1\s\+spark-node/d' /etc/hosts # 刪除配對到的行。 會多一行 spark 自訂的 IP
-
-for i in $(seq 1 $numNodes)
-do
+sed -i '/127.0.0.1\s\+spark-node/d' /etc/hosts
 cat >> /etc/hosts << EOF
-10.0.1.10$i spark-node$i spark-node$i
+10.0.1.101 spark-node1
+10.0.1.102 spark-node2
 EOF
-done
 
-# 解除防火牆與限制 ===============================================
 cat >> /etc/ssh/ssh_config << EOF
 Host *
-    StrictHostKeyChecking no
-    UserKnownHostsFile=/dev/null
+   StrictHostKeyChecking no
+   UserKnownHostsFile=/dev/null
 EOF
+
 
 cat >> /etc/sysctl.conf << EOF
 net.ipv6.conf.all.disable_ipv6 = 1
@@ -33,55 +23,54 @@ net.ipv6.conf.lo.disable_ipv6 = 1
 EOF
 sysctl -p
 
-swapoff -a # 臨時禁用虛擬內存 swap 
-sudo sed -i '/swap/s/^/#/' /etc/fstab # 永久禁用 swap 
+swapoff -a
+sed -i '/swap/s/^/#/' /etc/fstab
 
 # Disable firewall
-sudo apt -y install firewalld
-sudo systemctl stop firewalld
-sudo systemctl disable firewalld
+systemctl stop firewalld
+systemctl disable firewalld
 
-# Set SELinux in disabled mode | centos 才有
-# setenforce 0
-# sed -i 's/^SELINUX=enforcing$/SELINUX=disabled/' /etc/selinux/config
+# Set SELinux in disabled mode
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=disabled/' /etc/selinux/config
 
-# pyspark 環境 ===============================================
-sudo apt -y install python3-pip # sudo yum install -y python3
-
-sudo pip3 install pyspark 
-
-# spark & java 環境 ===============================================
-sudo chown -R vagrant:vagrant /opt/ # 把資料夾權限轉移，方便後續存取
-
-tar xf /share/packages/openlogic-openjdk-8u262-b10-linux-x64.tar.gz -C /opt
-tar xf /share/packages/spark-3.2.1-bin-hadoop3.2.tgz -C /opt
-
-mv -v /opt/spark-3.2{.1-bin-hadoop3.2,} # rename -> {} 裡的移掉
-find /opt -depth -type d -name openlogic-openjdk-8u262-b10-linux-64 -execdir mv {} /opt/jdk-8 \;
-
-cp /opt/spark-3.2/conf/spark-env.sh.template /opt/spark-3.2/conf/spark-env.sh
-# 設定 spark 參數 | 10.0.1.101
-cat >> /opt/spark-3.2/conf/spark-env.sh << EOF
-SPARK_MASTER_HOST=$masterIP
+tar xf /vagrant/packages/spark-2.2.3-bin-hadoop2.7.tgz -C /opt
+tar xf /vagrant/packages/hadoop-2.7.7.tar.gz -C /opt
+tar xf /vagrant/packages/jdk-8u201-linux-x64.tar.gz -C /opt
+mv -v /opt/spark-2.2{.3-bin-hadoop2.7,}
+mv -v /opt/hadoop-2.7{.7,}
+mv -v /opt/jdk1.8{.0_201,}
+cp -fv /vagrant/conf/{core-site.xml,hdfs-site.xml,mapred-site.xml,yarn-site.xml,slaves} /opt/hadoop-2.7/etc/hadoop
+cat >> /opt/spark-2.2/conf/spark-env.sh << EOF
+SPARK_MASTER_HOST=10.0.1.101
 SPARK_LOCAL_IP=$IP
+HADOOP_CONF_DIR=\$HADOOP_HOME/etc/hadoop
 EOF
 
-# 設定路徑
-# sudo chown -R vagrant:vagrant /etc/profile.d/ 
-# cat >> /etc/profile.d/custom.sh << EOF
-sudo chown -R vagrant:vagrant /home/vagrant # 把資料夾權限轉移，方便後續存取
-cat >> .bashrc << EOF
+chown -R vagrant:vagrant /opt/{spark-2.2,hadoop-2.7,jdk1.8}
+
+cat >> /etc/profile.d/custom.sh <<'EOF'
 #!/bin/bash
 ### JAVA ###
-export JAVA_HOME=/opt/jdk-8
+export JAVA_HOME=/opt/jdk1.8
 export CLASSPATH=.:$CLASSPATH:$JAVA_HOME/lib
 export PATH=$JAVA_HOME/bin:$PATH
 
-### Spark ###
-export SPARK_HOME=/opt/spark-3.2
-export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin
-export PYSPARK_PYTHON=/usr/bin/python3
-export SPARK_LOCAL_IP=localhost
+### HADOOP ###
+export HADOOP_HOME=/opt/hadoop-2.7
+export HADOOP_INSTALL=$HADOOP_HOME
+export HADOOP_MAPRED_HOME=$HADOOP_HOME
+export HADOOP_COMMON_HOME=$HADOOP_HOME
+export HADOOP_HDFS_HOME=$HADOOP_HOME
+export YARN_HOME=$HADOOP_HOME
+export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
+export PATH=$HADOOP_HOME/sbin:$HADOOP_HOME/bin:$PATH
+
+### SPARK ###
+export SPARK_HOME=/opt/spark-2.2
+export PATH=$SPARK_HOME/sbin:$SPARK_HOME/bin:$PATH
 EOF
 
-source .bashrc # 載入更新後的環境
+mkdir -pv /data/hadoop-data/{nn,snn,dn,mapred/{system,local}}
+chown -R vagrant:vagrant /data/hadoop-data/{nn,snn,dn,mapred/{system,local}}
+
